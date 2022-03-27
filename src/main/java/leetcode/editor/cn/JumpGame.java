430b7038ba4a2694117e3536b5849da0180b22ea
siwenboxue <siwenboxue@qq.com>
2022-03-27 15:54:21 +0800

    跳跃游戏解答

--- src/main/java/leetcode/editor/cn/JumpGame.java
+++ src/main/java/leetcode/editor/cn/JumpGame.java
@@ -35,6 +35,7 @@
 
 package leetcode.editor.cn;
 
+import com.quickcode.builder.Builder;
 import com.quickcode.debug.Recursion;
 import com.quickcode.predicate.Assert;
 
@@ -56,16 +57,58 @@ public class JumpGame {
 
     public static void main(String[] args) {
         Solution solution = getSolution();
-
+        System.out.println(solution.canJump(Builder.buildInt(2, 3, 1, 1, 4)));
+        System.out.println(solution.canJump(Builder.buildInt(3, 2, 1, 0, 4)));
         Assert.checkEqual();
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * 贪心解法
+     */
     class Solution {
         public boolean canJump(int[] nums) {
-
-            return true;
+            // 贪心的解法 每一步都在求局部的最优解 然后到达总体最优解
+            // 都是下标值
+            if (nums == null || nums.length == 0) return false;
+            int maxIndex = 0; // 总体上最大的长度
+            int currentIndex = 0; // 当前节点能走到的最大的长度
+            for (int i = 0; i < nums.length; i++) {
+                // 如果当前的路径长度已经大于最大能到达的路径长度,则表示到不了从当前的位置
+                if (i > maxIndex) return false;
+                // 更新局部值
+                currentIndex = i + nums[i];
+                // 更新全局最优值
+                if (currentIndex > maxIndex) {
+                    maxIndex = currentIndex;
+                }
+            }
+            return maxIndex >= nums.length - 1;
         }
+
+        /*
+         * dp解法
+         * */
+        public boolean canJumpDp(int[] nums) {
+            if (nums == null || nums.length == 0) return true;
+            boolean[] f = new boolean[nums.length];
+            // 遍历填充bool数组
+            f[0] = true; //
+            for (int i = 1; i < f.length; i++) {
+                // [0,i) 去找能不能到达f[i]
+                for (int j = 0; j < i; j++) {
+                    // 能走到f[i - j],并且(i - j)这一步所可以走到的最大路径 要覆盖到可以走到j
+                    if (f[j] && nums[j] >= i - j) {
+                        f[i] = true;
+                        break;
+                    }
+                }
+            }
+            return f[f.length - 1];
+        }
+
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
