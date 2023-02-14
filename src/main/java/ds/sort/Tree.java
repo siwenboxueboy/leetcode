fc4e94a6cdbbc8f2b2bd6b6fb15257b8321e5bb5
siwenboxue <siwenboxue@qq.com>
2023-02-14 20:45:31 +0800

    BST节点删除

--- src/main/java/ds/sort/Tree.java
+++ src/main/java/ds/sort/Tree.java
@@ -15,7 +15,6 @@ public class Tree {
     }
 
 
-
     // 先根遍历
     static void preTraverse(TreeNode treeNode) {
         if (treeNode != null) {
@@ -40,4 +39,52 @@ public class Tree {
             System.out.println("treeNode.val = " + treeNode.val);
         }
     }
+
+    private static TreeNode delBST(TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+        // 根节点的删除如何处理
+        if (root.val == target) {
+            return delBSTTreeNode(root);
+        }
+        delBSTTreeNode(root, target);
+        return root;
+    }
+
+    private static void delBSTTreeNode(TreeNode parent, int target) {
+        if (parent == null) return;
+        if (parent.left != null && parent.left.val == target) {
+            parent.left = delBSTTreeNode(parent.left);
+        } else if (parent.right != null && parent.right.val == target) {
+            parent.right = delBSTTreeNode(parent.right);
+        } else {
+            delBSTTreeNode(parent.left, target);
+            delBSTTreeNode(parent.right, target);
+        }
+    }
+
+    private static TreeNode delBSTTreeNode(TreeNode node) {
+        // 如果待删除节点左右子树都不剩，则删除之后直接为空
+        if (node.left == null && node.right == null) return null;
+        // 如果待删除节点的仅剩左子树或者右子树，则返回即可
+        if (node.left != null && node.right == null) {
+            return node.left;
+        }
+        if (node.left == null && node.right != null) {
+            return node.right;
+        }
+        // 以下为左右子树都有值的情况 这里处理的是找左子树的最小值（也可找右子树的最大值来替代）
+        // 其实就是找到左子树的最右边的节点
+        TreeNode leftParentMinNode = node;
+        TreeNode leftMinNode = node.left;
+        while (leftMinNode.left != null) {
+            leftParentMinNode = leftMinNode;
+            leftMinNode = leftMinNode.left;
+        }
+        node.val = leftMinNode.val;
+        leftParentMinNode = delBSTTreeNode(leftMinNode);
+        return node;
+    }
+
 }
